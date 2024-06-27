8c2bb77da155e2bdc1dca24e79ec68a54abc73cb
snudev-swkang <136809317+snudev-swkang@users.noreply.github.com>
2024-06-27 16:27:56 +0000

    Svelte structure

---
+++ ttp-vote/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<!--
+<span>
+    <img src="https://mmlab.snu.ac.kr/storage/app/public/theme/theme/together@together.1/d3/91/logoImage" alt="MMLAB LOGO"/>
+</span>
+-->
+<h1>Trusted Third Party Voter</h1>
+<h3>by @swkang, <a href="https://mmlab.snu.ac.kr" target="_blank">SNU MMLAB</a></h3>
