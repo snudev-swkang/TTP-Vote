6f982ad23c71b65b3e7335f5384670833549d7f6
snudev-swkang <136809317+snudev-swkang@users.noreply.github.com>
2024-06-28 02:15:22 +0000

    Add : Basic feature test

---
+++ ttp-vote/src/components/TTPVote.svelte
@@ -0,0 +1,31 @@
+<script>
+    // TTPVote.svelte
+    let participantsNum = 0;
+    let scores = [];
+    let average = 0;
+
+    function calculateAverage() {
+        const sum = scores.reduce((acc, score) => acc + score, 0);
+        average = sum / scores.length;
+    }
+</script>
+
+<label>
+    Number of Participants:
+    <input type="number" inputmode="numeric" bind:value={participantsNum} min="1" />
+</label>
+
+{#if participantsNum > 0}
+    <div>
+        {#each Array(participantsNum) as _, i}
+            <p>
+                <label>
+                    Score {i + 1}:
+                    <input type="number" inputmode="numeric" bind:value={scores[i]} min="1" />
+                </label>
+            </p>
+        {/each}
+        <button on:click={calculateAverage}>Calculate Average</button>
+        <p>Average: {average.toFixed(2)}</p>
+    </div>
+{/if}
--- ttp-vote/src/routes/+page.svelte
+++ ttp-vote/src/routes/+page.svelte
@@ -1,7 +1,17 @@
+<script>
+    import TTPVote from "../components/TTPVote.svelte";
+</script>
+
+
 <!--
 <span>
     <img src="https://mmlab.snu.ac.kr/storage/app/public/theme/theme/together@together.1/d3/91/logoImage" alt="MMLAB LOGO"/>
 </span>
 -->
-<h1>Trusted Third Party Voter</h1>
-<h3>by @swkang, <a href="https://mmlab.snu.ac.kr" target="_blank">SNU MMLAB</a></h3>
+<h2>Trusted Third Party Voter</h2>
+<p>by @swkang, <a href="https://mmlab.snu.ac.kr" target="_blank">SNU MMLAB</a></p>
+<br/>
+<p>
+    <TTPVote message="This is a message from the main page" />
+</p>
+
